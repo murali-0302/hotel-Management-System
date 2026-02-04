5c46a77594c1ac721fa0bbe2b014478b53173cf4
muralidharan <jmuralidharan.03@gmail.com>
2026-02-04 13:03:35 +0530

    Resolve merge conflicts

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   const menuBtn = document.getElementById("menuBtn");
   const sidebar = document.getElementById("sidebarPanel");
   const overlay = document.getElementById("overlay");
@@ -24,23 +23,3 @@
       }
     });
   });
-=======
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", function (e) {
-
-    const href = this.getAttribute("href");
-
-    // Ignore logout or external links
-    if (!href || href === "#" || href.startsWith("http")) return;
-
-    e.preventDefault();
-
-    const page = document.querySelector(".page");
-    page.classList.add("fade-exit");
-
-    setTimeout(() => {
-      window.location.href = href;
-    }, 300);
-  });
-});
->>>>>>> 484c6510888383d3b60300bdec6210e080812d48
