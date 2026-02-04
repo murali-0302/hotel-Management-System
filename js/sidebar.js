3f371818ba4dd2b8622f303b3df8b8ef4480f2a4
muralidharan <jmuralidharan.03@gmail.com>
2026-02-04 12:45:33 +0530

    hi2

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,18 +1,25 @@
-document.querySelectorAll("a").forEach(link => {
-  link.addEventListener("click", function (e) {
+  const menuBtn = document.getElementById("menuBtn");
+  const sidebar = document.getElementById("sidebarPanel");
+  const overlay = document.getElementById("overlay");
 
-    const href = this.getAttribute("href");
-
-    // Ignore logout or external links
-    if (!href || href === "#" || href.startsWith("http")) return;
-
-    e.preventDefault();
-
-    const page = document.querySelector(".page");
-    page.classList.add("fade-exit");
-
-    setTimeout(() => {
-      window.location.href = href;
-    }, 300);
+  menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("show");
+    overlay.classList.toggle("show");
+    menuBtn.classList.toggle("active");
   });
-});
+
+  overlay.addEventListener("click", () => {
+    sidebar.classList.remove("show");
+    overlay.classList.remove("show");
+    menuBtn.classList.remove("active");
+  });
+
+  document.querySelectorAll(".sidebar a").forEach(link => {
+    link.addEventListener("click", () => {
+      if (window.innerWidth <= 480) {
+        sidebar.classList.remove("show");
+        overlay.classList.remove("show");
+        menuBtn.classList.remove("active");
+      }
+    });
+  });
